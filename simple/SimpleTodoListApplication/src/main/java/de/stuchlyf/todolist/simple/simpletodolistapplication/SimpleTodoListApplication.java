9b47875eac81d86cc768a9951fae58d44f442ee6
Florian Stuchly <florian.stuchly@gmail.com>
2022-10-24 12:33:39 +0200

    Initialize Simple Difficulty

---
+++ simple/SimpleTodoListApplication/src/main/java/de/stuchlyf/todolist/simple/simpletodolistapplication/SimpleTodoListApplication.java
@@ -0,0 +1,13 @@
+package de.stuchlyf.todolist.simple.simpletodolistapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleTodoListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleTodoListApplication.class, args);
+    }
+
+}
---
+++ simple/SimpleTodoListApplication/src/test/java/de/stuchlyf/simpletodolistapplication/SimpleTodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package de.stuchlyf.simpletodolistapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleTodoListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
